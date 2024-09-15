b8fb4519829cf71425e59e44fe1eb040a8dc9ea1
helena zhou <66381768+zhouhelena@users.noreply.github.com>
2024-09-15 08:55:40 -0400

    scripty oops

--- teacher-frontend/app/components/ui/hero-parallax.tsx
+++ teacher-frontend/app/components/ui/hero-parallax.tsx
@@ -110,13 +110,13 @@ export const Header = () => {
   return (
     <div className="max-w-7xl relative mx-auto py-20 md:py-40 px-4 w-full  left-0 top-0">
       <h1 className="text-2xl md:text-7xl font-bold dark:text-white">
-        Scrippy
+        Scripty
       </h1>
       <p className="max-w-2xl text-base md:text-xl mt-8 dark:text-neutral-200">
         Seamless development setup for education
       </p>
       <SignInButton >
-        <Button className="mt-6">Sign In</Button>
+        <Button className="mt-6">Get Started</Button>
       </SignInButton>
     </div>
   );
--- teacher-frontend/app/root.tsx
+++ teacher-frontend/app/root.tsx
@@ -8,7 +8,7 @@ import "./tailwind.css";
 
 export const meta: MetaFunction = () => ([{
   charset: "utf-8",
-  title: "Scrippy",
+  title: "Scripty",
   viewport: "width=device-width,initial-scale=1",
 }]);
 
